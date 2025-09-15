8921d4e6d31e1f82412716295a8024ed869a2438
tu <t.u.9293991@gmail.com>
2025-09-15 18:34:39 +0900

    commit

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,4 +1,4 @@
-// Import the functions you need from the SDKs you need
+"use client"
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
