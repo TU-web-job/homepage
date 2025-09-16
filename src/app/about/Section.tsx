9088ebaab01ba5c8b7dd9ac308e7e949a55f3712
tu <t.u.9293991@gmail.com>
2025-09-16 20:57:59 +0900

    section

--- src/app/about/Section.tsx
+++ src/app/about/Section.tsx
@@ -1,15 +1,11 @@
 "use client"
 import { GiSoccerKick } from "react-icons/gi";
-import dynamic from "next/dynamic";
-
-const Access = dynamic(() => import("@/app/about/Access"), {
-    ssr: false,
-});
+import Access from "@/app/about/Access";
 
 export default function Section(){
     return(
         <div className="flex flex-col p-4 justify-center items-center w-full mt-4">
-            <h1 className="flex flex-row gap-2 text-xl sm:text-2xl md:text-4xl font-bold">自己紹介<GiSoccerKick /></h1>
+            <h1 className="flex flex-row gap-2 text-xl sm:text-2xl md:text-4xl font-bold">スクール概要<GiSoccerKick /></h1>
             <div className="flex flex-row mt-4 border-2 border-gray-400 p-4 w-full">
                 <div className="w-[50%] p-2">
                     <h3 className="border-b-2 border-gray-200 font-semibold mb-2">挨拶</h3>
@@ -44,12 +40,119 @@ export default function Section(){
             </div>
             <div className="flex flex-row mt-4 border-2 border-gray-400 p-4 w-full">
                 <div className="w-[50%] p-2">
-                    <Access />
+                        <p className="border-b-2 border-gray-200">レギュラーシーズン</p>
+                        <table className="table w-full p-2">
+                            <thead>
+                            <tr className="border-1 border-gray-200 bg-gray-200">
+                                <th>学年</th>
+                                <th>月</th>
+                                <th>火</th>
+                                <th>水</th>
+                                <th>木</th>
+                                <th>金</th>
+                                <th>土</th>
+                                <th>日</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <tr className="border-1 border-gray-200 justify-center items-center">
+                                <td>1.2年生</td>
+                                <td>⚪︎</td>
+                                <td>-</td>
+                                <td>-</td>
+                                <td>-</td>
+                                <td>⚪︎</td>
+                                <td>-</td>
+                                <td>-</td>
+                            </tr>
+                            <tr className="border-1 border-gray-200 justify-center items-center">
+                                <td>3.4年生</td>
+                                <td>-</td>
+                                <td>⚪︎</td>
+                                <td>⚪︎</td>
+                                <td>-</td>
+                                <td>⚪︎</td>
+                                <td>-</td>
+                                <td>-</td>
+                            </tr>
+                            <tr className="border-1 border-gray-200 justify-center items-center">
+                                <td>5.6年生</td>
+                                <td>⚪︎</td>
+                                <td>⚪︎</td>
+                                <td>-</td>
+                                <td>⚪︎</td>
+                                <td>-</td>
+                                <td>-</td>
+                                <td>-</td>
+                            </tr>
+                            </tbody>
+                        </table>
+                        <p className="border-b-2 border-gray-200 mt-2">バケーションシーズン</p>
+                        <table className="table flex justify-center items-center w-full p-2">
+                        <thead>
+                            <tr className="border-1 border-gray-200 bg-gray-200">
+                                <th>学年</th>
+                                <th>月</th>
+                                <th>火</th>
+                                <th>水</th>
+                                <th>木</th>
+                                <th>金</th>
+                                <th>土</th>
+                                <th>日</th>
+                            </tr>
+                            </thead>
+                            <tbody className="items-center justify-center">
+                            <tr className="border-1 border-gray-200">
+                                <td>1.2年生</td>
+                                <td>⚪︎</td>
+                                <td>-</td>
+                                <td>⚪︎</td>
+                                <td>⚪︎</td>
+                                <td>-</td>
+                                <td>-</td>
+                                <td>-</td>
+                            </tr>
+                            <tr className="border-1 border-gray-200">
+                                <td>3.4年生</td>
+                                <td>⚪︎</td>
+                                <td>⚪︎</td>
+                                <td>-</td>
+                                <td>⚪︎</td>
+                                <td>-</td>
+                                <td>⚪︎</td>
+                                <td>-</td>
+                            </tr>
+                            <tr className="border-1 border-gray-200">
+                                <td>5.6年生</td>
+                                <td>-</td>
+                                <td>⚪︎</td>
+                                <td>⚪︎</td>
+                                <td>-</td>
+                                <td>⚪︎</td>
+                                <td>⚪︎</td>
+                                <td>-</td>
+                            </tr>
+                            </tbody>
+                        </table>
                 </div>
+                <div className="w-[50%] p-2">
+                    <h3 className="border-b-2 border-gray-200 font-semibold mb-2">開催日</h3>
+                    <p>レギュラーシーズン</p>
+                    <p>曜日 : </p>
+                    <p>時間 : </p>
+                    <p>バケーションシーズン(春、夏、冬)</p>
+                    <p>曜日 : </p>
+                    <p>時間 : </p>
+                </div>
+            </div>
+            <div className="flex flex-row mt-4 border-2 border-gray-400 p-4 w-full">
                 <div className="w-[50%] p-2">
                     <h3 className="border-b-2 border-gray-200 font-semibold mb-2">活動場所 / アクセス</h3>
                     <p>活動場所 : 千葉県船橋市習志野台7-5-1</p>
                     <p>アクセス : 船橋日大前駅 徒歩10分</p>
+                </div>
+                <div className="w-[50%] p-2">
+                    <Access />
                 </div>
             </div>
         </div>
