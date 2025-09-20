aa106eb99a8f58110556b5163229b9c4a9167925
tu <t.u.9293991@gmail.com>
2025-09-21 04:13:40 +0900

    admin

--- src/app/admin/Admin.tsx
+++ src/app/admin/Admin.tsx
@@ -37,17 +37,17 @@ export default function Admin(){
         <div className="flex flex-col justify-center items-center w-full p-4">
             <h2 className="text-2xl font-bold text-gray-600 mb-2">管理者ページ</h2>
             <section className="flex flex-row justify-center items-center">
-                <label className="p-2 m-2"><input type="radio" name="list" onClick={() => setView("contacts")} required />Contact List</label>
+                <label className="p-2 m-2"><input type="radio" name="list" onClick={() => setView("contacts")} />Contact List</label>
                 <label className="p-2 m-2"><input type="radio" name="list" onClick={() => setView("trial")} />Trial List</label>
             </section>
             {view === "contacts" && (
-                <div className="w-full border border-gray-400 p-2 mb-4">
+                <div className="w-full border border-gray-800 bg-gray-300 p-2 mb-4">
                     <h3 className="font-semibold text-xl p-2 mb-4">お問い合わせ一覧</h3>
                     {contacts.map(contact => (
-                        <ul key={contact.id} className="flex flex-col p-2 mb-4">
-                            <li>Name : {contact.name}</li>
-                            <li>E-Mail : {contact.email}</li>
-                            <li>Message : {contact.message}</li>
+                        <ul key={contact.id} className="border rounded-lg shadow-md bg-white grid gap-2 p-2 mb-4">
+                            <li><span className="font-semibold">Name : </span>{contact.name}</li>
+                            <li><span className="font-semibold">E-Mail : </span>{contact.email}</li>
+                            <li><span className="font-semibold">Message : </span>{contact.message}</li>
                         </ul>
                     ))}
                 </div>
@@ -57,17 +57,16 @@ export default function Admin(){
                 <h3 className="font-semibold text-xl p-2 mb-4">体験者一覧</h3>
                 {trial.map((item) => (
                     <ul key={item.id} className="flex flex-col p-2 mb-4 border-b border-gray-300">
-                        <li>体験者名 : {item.trialName}</li>
-                        <li>保護者名 : {item.parentName}</li>
-                        <li>学年 : {item.grade}</li>
-                        <li>体験希望日 : {item.trialDate}</li>
-                        <li>連絡先 : {item.mail}</li>
-                        <li>備考 : {item.message}</li>
+                        <li><span className="font-semibold">体験者名 : </span>{item.trialName}</li>
+                        <li><span className="font-semibold">保護者名 : </span>{item.parentName}</li>
+                        <li><span className="font-semibold">学年 : </span>{item.grade}</li>
+                        <li><span className="font-semibold">体験希望日 : </span>{item.trialDate}</li>
+                        <li><span className="font-semibold">連絡先 : </span>{item.mail}</li>
+                        <li><span className="font-semibold">備考 : </span>{item.message}</li>
                     </ul>
                 ))}
             </div>
             )}
-            
         </div>
     );
 }
