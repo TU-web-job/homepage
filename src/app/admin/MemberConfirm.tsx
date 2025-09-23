68cfe7edad9c6ac3f31044478c102286f19e8bf4
tu <t.u.9293991@gmail.com>
2025-09-23 22:20:10 +0900

    admin

--- src/app/admin/MemberConfirm.tsx
+++ src/app/admin/MemberConfirm.tsx
@@ -49,53 +49,51 @@ const onSubmit = async (data: memberData) => {
 
     return(
         <div className="flex flex-col w-full p-4 m-2 rounded">
-            <h1>スクール生登録</h1>
+            <h1 className="font-bold jutify-center items-center">スクール生登録</h1>
             <form onSubmit={handleSubmit(onSubmit)} className="p-2">
                 <nav>
                     <div>
-                        <p>名字</p>
-                        <input {...register("firstName", {required: "名字を入力してください。"})} className="border"/>
+                        <p>名字 : <input {...register("firstName", {required: "名字を入力してください。"})} className="border border-gray-300 m-2 rounded"/></p>
+                        
                         {errors.firstName && <p className="text-red-500 text-sm">{errors.firstName.message}</p>}
-                    </div>
-                    <div>
-                        <p>名前</p>
-                        <input {...register("lastName", {required: "名前を入力してください。"})} className="border"/>
+                        <p>名前 : <input {...register("lastName", {required: "名前を入力してください。"})} className="border border-gray-300 m-2 rounded"/></p>
+                        
                         {errors.lastName && <p className="text-red-500 text-sm">{errors.lastName.message}</p>}
                     </div>
                 </nav>
                 <nav>
-                    <p>保護者名</p>
-                    <input {...register("parentName", {required: "保護者名を入力してください。"})} className="border" />
+                    <p>保護者名 : <input {...register("parentName", {required: "保護者名を入力してください。"})} className="border border-gray-300 m-2 rounded" /></p>
+                    
                     {errors.parentName && <p className="text-red-500 text-sm">{errors.parentName.message}</p>}
                 </nav>
                 <nav>
-                    <p>年齢</p>
-                    <input type="date" {...register("age", {required: "年齢を入力してください。"})} className="border" />
+                    <p>年齢 : <input type="date" {...register("age", {required: "年齢を入力してください。"})} className="border border-gray-300 m-2 rounded" /></p>
+                    
                     {errors.age && <p className="text-red-500 text-sm">{errors.age.message}</p>}
                 </nav>
                 <nav>
-                    <p>在籍グループ</p>
-                    <select {...register("grade", {required:"選択してください。"})} className="border border-gray-300 p-2 rounded">
+                    <p>在籍グループ : <select {...register("grade", {required:"選択してください。"})} className="border border-gray-300 p-2 m-2 rounded">
                         {gradeType.map((item) => (
                             <option key={item} value={item}>{item}</option>
                         ))}
-                    </select>
+                    </select></p>
+                    
                     {errors.grade && <p className="text-red-500 text-sm">{errors.grade.message}</p>}
                 </nav>
                 <nav>
-                    <p>郵便番号</p>
-                    <input {...register("postCode", {required: "郵便番号を入力してください。",pattern: {value:/^\d{3}-\d{4}$/,message:"xxx-xxxxの形式で入力してください。"}})} className="border w-[80px]" placeholder="xxx-xxxx" inputMode="numeric"/>
-                    <p>住所</p>
-                    <input {...register("address", {required: "住所を入力してください。"})} className="border" placeholder="市区町村まで"/>
+                    <p>郵便番号 <input {...register("postCode", {required: "郵便番号を入力してください。",pattern: {value:/^\d{3}-\d{4}$/,message:"xxx-xxxxの形式で入力してください。"}})} className="border border-gray-300 w-[80px] m-2 rounded" placeholder=" xxx-xxxx" inputMode="numeric"/>
+                    <p>住所 <input {...register("address", {required: "住所を入力してください。"})} className="border border-gray-300 m-2 rounded" placeholder="市区町村まで"/></p></p>
+                    
+                    
                 </nav>
                 <nav>
-                    <p>電話番号</p>
-                    <input {...register("phone", {required:"電話番号を入力してください。"})} className="border" placeholder="xxx-xxxx-xxxx" />
+                    <p>電話番号 <input {...register("phone", {required:"電話番号を入力してください。"})} className="border border-gray-300 m-2 rounded" placeholder="xxx-xxxx-xxxx" /></p>
+                    
                     {errors.phone && <p className="text-red-500 text-sm">{errors.phone.message}</p>}
                 </nav>
                 <nav>
-                    <p>E-Mail</p>
-                    <input type="email" {...register("mail", {required:"メールアドレスを入力してください。"})} className="border" />
+                    <p>E-Mail <input type="email" {...register("mail", {required:"メールアドレスを入力してください。"})} className="border border-gray-300 m-2 rounded" /></p>
+                    
                     {errors.mail && <p className="text-red-500 text-sm">{errors.mail.message}</p>}
                 </nav>
                 <button type="submit" className="bg-blue-500 text-white p-2 rounded">登録</button>
